f8e53c46bc3f04449fe62e28e19ff61d6e311894
Daniel von Essen <daniel@vonessen.eu>
2023-10-08 07:59:03 +0200

    feat: :sparkles: Added nur to nixpkgs configuration.

--- modules/nixpkgs/config.nix
+++ modules/nixpkgs/config.nix
@@ -4,4 +4,9 @@
     "/share"
     "/share/bash-completion"
   ];
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
 }
