f190ca44430fb3eda1a4bac4fdf741fbd96f52e5
Daniel von Eßen <daniel@vonessen.eu>
2023-10-17 06:13:23 +0200

    feat: :sparkles: Added all brew casks and zapped all other away.

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -7,34 +7,30 @@
     enable = true;
     caskArgs = {
       appdir = "~/Applications";
-      require_sha = true;
     };
     casks = [
-      # "amazon-workspaces"
-      # "apache-directory-studio"
-      # "bitwarden"
-      # "caffeine"
-      # "docker"
-      # "ferdium"
+      "amazon-workspaces"
+      "bitwarden"
+      "caffeine"
+      "docker"
+      "ferdium"
       "firefox"
-      # "font-fira-code"
-      # "inkscape"
-      # "keepassxc"
-      # "libreoffice"
-      # "nextcloud"
-      # "origin"
-      # "rectangle"
-      # "spotify"
-      # "steam"
-      # "utm"
-      # "visual-studio-code"
-      # "wireshark"
+      "inkscape"
+      "keepassxc"
+      "libreoffice"
+      "nextcloud"
+      "onedrive"
+      "rectangle"
+      "spotify"
+      "steam"
+      "utm"
+      "visual-studio-code"
     ];
     global.autoUpdate = false;
     onActivation = {
       autoUpdate = false;
       upgrade = true;
-      cleanup = "none"; # Set it to zap, as soon as everything is in home-manager/nix-darwin.
+      cleanup = "zap";
     };
   };
 }
