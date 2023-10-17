47979d51e047d38a887586b16b69edd75268606f
Daniel von Eßen <daniel@vonessen.eu>
2023-10-17 06:14:10 +0200

    feat: :sparkles: Added more kubernetes packages.

--- home/k8s.nix
+++ home/k8s.nix
@@ -1,4 +1,18 @@
-{...}: {
+{
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    kubernetes-helm
+    kube-linter
+    kubectx
+    kubectl
+    kustomize
+    minio-client
+    stern
+    velero
+  ];
+
   programs.k9s = {
     enable = true;
     settings = {
