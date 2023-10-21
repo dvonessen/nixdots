cb1cb22cb1119b450c8092d72034bff88688db64
Daniel von Eßen <daniel@vonessen.eu>
2023-10-21 06:57:40 +0200

    feat: :sparkles: Added nix configuration globally.

---
+++ darwin/nix.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  # enable flakes globally
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Follow the instance of this flakes nixpkgs
+  nix.package = pkgs.nix;
+
+  # Enable nix-index and it's command-not-found helper
+  programs.nix-index.enable = true;
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    options = lib.mkDefault "--delete-older-than 1w";
+  };
+
+  # Manual optimise storage: nix-store --optimise
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+}
