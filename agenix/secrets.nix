327ca70c2576fd9766cec872c430770b31f3afda
Daniel von Eßen <daniel@vonessen.eu>
2023-10-23 06:54:54 +0200

    docs: :memo: Added some comments to secrets.nix

--- agenix/secrets.nix
+++ agenix/secrets.nix
@@ -3,9 +3,13 @@ let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyOtbyOasyBrcr4PUsalys0QyVas6zz0n4UEl1np6Cg";
   users = [agenix];
 in {
+  # AWS CLI credentials
   "awscli-credentials.age".publicKeys = users;
+  # SSH client configuration
   "ssh-clients-config.age".publicKeys = users;
+  # Docker client.json
   "docker-config-json.age".publicKeys = users;
+  # WireGuard VPN configuration file
   "wg-danielmacbook.age".publicKeys = users;
   "wg-admin.age".publicKeys = users;
   "wg-vpn.mobileconfig.age".publicKeys = users;
