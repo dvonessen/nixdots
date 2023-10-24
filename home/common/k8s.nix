1c70893716d7a39a784e4bc58ca5bbcd08f53d45
Daniel von Eßen <daniel@vonessen.eu>
2023-10-24 20:59:16 +0200

    refactor: :recycle: Sort k8s packages list.

--- home/common/k8s.nix
+++ home/common/k8s.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    kubernetes-helm
     kube-linter
-    kubectx
     kubectl
+    kubectx
+    kubernetes-helm
     kustomize
     minio-client
     stern
