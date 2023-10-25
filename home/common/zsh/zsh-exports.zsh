292e9a673bdbf4bc09f6c4c6d03d3a424a6d4a2c
Daniel von Eßen <daniel@vonessen.eu>
2023-10-25 16:44:47 +0200

    feat: :sparkles: Export homebrew environment if homebrew is installed

--- home/common/zsh/zsh-exports.zsh
+++ home/common/zsh/zsh-exports.zsh
@@ -71,3 +71,9 @@ if command -v fuck &> /dev/null; then
     # See https://github.com/nvbn/thefuck/issues/1153
     export THEFUCK_EXCLUDE_RULES=fix_file
 fi
+
+if [ -x /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
