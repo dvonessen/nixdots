314f2147875c07417ad943508a43096013344aa7
Daniel von Eßen <daniel@vonessen.eu>
2023-11-06 16:31:13 +0100

    feat: :sparkles: Wezterm now is aware of system wide dark/light mode.

---
+++ home/common/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Vs Code Dark+ (Gogh)'
+  else
+    return 'Vs Code Light+ (Gogh)'
+  end
+end
+
+
+-- For example, changing the color scheme:
+return {
+  scrollback_lines = 50000,
+  color_scheme = scheme_for_appearance(get_appearance()),
+}
