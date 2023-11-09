904dcf0280b35403fc4e54a29f2688803a2bf323
Daniel von Eßen <daniel@vonessen.eu>
2023-11-09 13:36:00 +0100

    feat: :sparkles: Added vscode-theme to nvim.

--- home/common/nvim.nix
+++ home/common/nvim.nix
@@ -2,7 +2,17 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  vscode-theme = pkgs.vimUtils.buildVimPlugin {
+    name = "vscode";
+    src = pkgs.fetchFromGitHub {
+      owner = "Mofiqul";
+      repo = "vscode.nvim";
+      rev = "563e3f671543ba14f23ccb57020a428add640d02";
+      hash = "sha256-rY/oE1NjqVskaeU5eaVQiy08uz3Bg/Z031wVjy3nPBI=";
+    };
+  };
+in {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -12,6 +22,7 @@
     plugins = with pkgs.vimPlugins; [
       vim-airline
       vim-code-dark
+      vscode-theme
     ];
     extraConfig = builtins.concatStringsSep "\n" [
       (builtins.readFile ./vim/vimrc.vim)
