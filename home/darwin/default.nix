ff1f049390bf644f5eda59113a92b555e8ae0cb4
Daniel von Eßen <daniel@vonessen.eu>
2023-11-09 10:27:24 +0100

    refactor: Improved Home Manager Trampoline apps.

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -2,22 +2,19 @@
   config,
   pkgs,
   lib,
-  mac-app-util,
   ...
-}: {
+} @ inputs: {
+  # For home-manager controlled apps. See
+  # https://github.com/nix-community/home-manager/issues/1341
   home.activation = {
     trampolineApps = let
-      apps = pkgs.buildEnv {
-        name = "home-manager-applications";
-        paths = config.home.packages;
-        pathsToLink = "/Applications";
-      };
-      mac-app-util_ = mac-app-util.packages.${pkgs.stdenv.system}.default;
-    in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      fromDir="${apps}/Applications/"
-      toDir="$HOME/Applications/Home Manager Trampolines"
-      ${mac-app-util_}/bin/mac-app-util sync-trampolines "$fromDir" "$toDir"
-  '';
+      mac-app-util = inputs.mac-app-util.packages.${pkgs.stdenv.system}.default;
+    in
+      lib.hm.dag.entryAfter ["writeBoundary"] ''
+        fromDir="$HOME/Applications/Home Manager Apps"
+        toDir="$HOME/Applications/Home Manager Trampolines"
+        ${mac-app-util}/bin/mac-app-util sync-trampolines "$fromDir" "$toDir"
+      '';
   };
   imports = [
     ./applications.nix
