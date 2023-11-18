1dcd8165e22fd7d6c0aa2ecee4b5cb0ee086bcad
Daniel von Eßen <daniel@vonessen.eu>
2023-11-18 07:27:31 +0100

    Restructured autostart to work with Home Manager Trampoline Apps

--- home/darwin/autostart.nix
+++ home/darwin/autostart.nix
@@ -6,7 +6,7 @@
       enable = true;
       config = {
         Disable = false;
-        ProgramArguments = ["/usr/bin/open" "-n" "${service.path or "${config.home.homeDirectory}/Applications/"}${service.app}"];
+        ProgramArguments = ["/usr/bin/open" "${service.path or "${config.home.homeDirectory}/Applications/Home Manager Trampolines/"}/${service.app}"];
         RunAtLoad = true;
         StandardErrorPath = /. + "/tmp/${name}.err.log";
         StandardOutPath = /. + "/tmp/${name}.out.log";
@@ -17,22 +17,36 @@ in {
   launchd = {
     enable = true;
     agents = launchAgentPlistConfig {
-      "eu.vonessen.firefox" = {app = "Firefox.app";};
-      "eu.vonessen.bitwarden" = {app = "Bitwarden.app";};
-      "eu.vonessen.rectangle" = {app = "Rectangle.app";};
-      "eu.vonessen.caffeine" = {app = "Caffeine.app";};
-      "eu.vonessen.docker" = {app = "Docker.app";};
-      "eu.vonessen.ferdium" = {app = "Ferdium.app";};
-      "eu.vonessen.spotify" = {app = "Spotify.app";};
-      "eu.vonessen.vscode" = {app = "Visual Studio Code.app";};
+      "eu.vonessen.bitwarden" = {
+        path = "${config.home.homeDirectory}/Applications/";
+        app = "Bitwarden.app";
+      };
+      "eu.vonessen.caffeine" = {
+        path = "${config.home.homeDirectory}/Applications/";
+        app = "Caffeine.app";
+      };
       "eu.vonessen.calendar" = {
         app = "Calendar.app";
         path = "/System/Applications/";
       };
+      "eu.vonessen.docker" = {
+        path = "${config.home.homeDirectory}/Applications/";
+        app = "Docker.app";
+      };
+      "eu.vonessen.ferdium" = {
+        path = "${config.home.homeDirectory}/Applications/";
+        app = "Ferdium.app";
+      };
+      "eu.vonessen.firefox" = {app = "Firefox.app";};
+      "eu.vonessen.keepassxc" = {app = "KeePassXC.app";};
       "eu.vonessen.mail" = {
         app = "Mail.app";
         path = "/System/Applications/";
       };
+      "eu.vonessen.rectangle" = {app = "Rectangle.app";};
+      "eu.vonessen.spotify" = {app = "Spotify.app";};
+      "eu.vonessen.vscode" = {app = "Visual Studio Code.app";};
+      "eu.vonessen.wezterm" = {app = "WezTerm.app";};
     };
   };
 }
