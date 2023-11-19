60af26b1a0ff306083853d3bcbc858142e46787c
Daniel von Eßen <daniel@vonessen.eu>
2023-11-19 07:46:35 +0100

    refactor: :art: Done linting.

--- home/common/python.nix
+++ home/common/python.nix
@@ -1,16 +1,18 @@
 {pkgs, ...}: let
   python311WithPackages = with pkgs; [
-    (python311.withPackages (ps: with ps; [
-      ipython
-      pip
-      pyyaml
-    ]))
+    (python311.withPackages (ps:
+      with ps; [
+        ipython
+        pip
+        pyyaml
+      ]))
   ];
 in {
-  home.packages = with pkgs; [
-    pipenv
-  ]
-  ++ python311WithPackages;
+  home.packages = with pkgs;
+    [
+      pipenv
+    ]
+    ++ python311WithPackages;
 
   programs.pyenv = {
     enable = true;
