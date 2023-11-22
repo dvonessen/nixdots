be6179cee1c753db47ea971b6aa6d54cd944eb4c
Daniel von Eßen <daniel@vonessen.eu>
2023-11-22 16:38:30 +0100

    feat: :sparkles: Added keybindings.

--- home/linux/gnome/peripherals.nix
+++ home/linux/gnome/peripherals.nix
@@ -19,13 +19,24 @@
 
     # Keyboard shortcuts
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      home = ["<Super>d"];
-      search = ["<Super>space"];
+      home = ["<Alt>d"];
+      search = ["<Alt>space"];
     };
     "org/gnome/desktop/peripherals/keyboard" = {
       repeat = true;
       repeat-interval = lib.hm.gvariant.mkUint32 15;
       delay = lib.hm.gvariant.mkUint32 300;
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Alt>q"];
+      switch-group = ["<Alt>less"];
+      switch-group-backward = ["<Shift><Alt>less"];
+      activate-window-menu = ["<Super>space"];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Alt><Super>t";
+      command = "wezterm";
+      name = "terminal";
     };
   };
 }
