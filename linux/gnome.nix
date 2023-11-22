62c746b0bdd173d870aa246207d90666a7a421b1
Daniel von Eßen <daniel@vonessen.eu>
2023-11-22 11:50:02 +0100

    feat: :sparkles: Configured gnome service and excluded all packages at system level.

--- linux/gnome.nix
+++ linux/gnome.nix
@@ -25,15 +25,48 @@
     };
 
     gnome = {
-      core-utilities.enable = false;
+      core-utilities.enable = true;
       gnome-user-share.enable = false;
       gnome-online-accounts.enable = false;
       gnome-browser-connector.enable = false;
       games.enable = false;
+      gnome-initial-setup.enable = false;
+      gnome-remote-desktop.enable = false;
     };
   };
 
   environment.gnome.excludePackages = with pkgs; [
+    gnome-connections
+    gnome-console
+    gnome-photos
+    gnome-text-editor
     gnome-tour
+    gnome.baobab
+    gnome.cheese
+    gnome.eog
+    gnome.epiphany
+    gnome.evince
+    gnome.geary
+    gnome.gnome-calculator
+    gnome.gnome-calendar
+    gnome.gnome-characters
+    gnome.gnome-clocks
+    gnome.gnome-contacts
+    gnome.gnome-disk-utility
+    gnome.gnome-font-viewer
+    gnome.gnome-logs
+    gnome.gnome-maps
+    gnome.gnome-music
+    gnome.gnome-system-monitor
+    gnome.gnome-weather
+    gnome.seahorse
+    gnome.simple-scan
+    gnome.totem
+    gnome.yelp
+    gnome.file-roller
+    gnome.nautilus
+    gnome.sushi
+    gnome.gnome-shell-extensions
+    orca
   ];
 }
