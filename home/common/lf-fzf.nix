8751b428ee17bd842df555de8de91e93ea52f418
Daniel von Eßen <daniel@vonessen.eu>
2023-12-13 10:46:03 +0100

    feat: :sparkles: Added some configuration options and icon set for lf.

--- home/common/lf-fzf.nix
+++ home/common/lf-fzf.nix
@@ -1,9 +1,16 @@
 {pkgs, config, ...}: {
-  home.file.lf-fzf-preview = {
-    enable = true;
-    executable = true;
-    target = ".config/lf/preview.sh";
-    source = ./lib/lf-fzf-preview.sh;
+  home.file = {
+    lf-fzf-preview = {
+      enable = true;
+      executable = true;
+      target = ".config/lf/preview.sh";
+      source = ./lib/lf-fzf-preview.sh;
+    };
+    lf-icons = {
+      enable = true;
+      target = ".config/lf/icons";
+      source = ./lib/lf-icons;
+    };
   };
 
   home.packages = with pkgs; [
@@ -33,11 +40,16 @@
       enable = true;
       previewer.source = "${config.home.homeDirectory}/${config.home.file.lf-fzf-preview.target}";
       settings = {
+        shell = "zsh";
         hidden = true;
         drawbox = true;
         icons = true;
         ignorecase = true;
         sixel = true;
+        dupfilefmt = "%b.%n%e";
+        relativenumber = true;
+        scrolloff = 10;
+        info = "size";
       };
       extraConfig = ''
         $mkdir -p ${config.xdg.cacheHome}/lf
@@ -53,7 +65,6 @@
         "--preview '${config.home.homeDirectory}/${config.home.file.lf-fzf-preview.target} {}'"
         "--cycle"
         "--border=bold"
-
       ];
     };
   };
