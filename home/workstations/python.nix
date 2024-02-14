428b675e75d4660ddadafd70725bd1edfced2b0a
Daniel von Eßen <daniel@vonessen.eu>
2024-02-14 16:40:23 +0100

    feat: Added rye.

--- home/workstations/python.nix
+++ home/workstations/python.nix
@@ -11,6 +11,7 @@ in {
   home.packages = with pkgs;
     [
       pipenv
+      rye
     ]
     ++ python311WithPackages;
 
@@ -19,4 +20,17 @@ in {
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
+
+  programs.bash.initExtra = ''
+    source ${pkgs.rye}/share/bash-completion/completions/rye.bash
+    if [ -f "$HOME/.rye/env" ]; then
+      source "$HOME/.rye/env"
+    fi
+  '';
+  programs.zsh.initExtra = ''
+    source ${pkgs.rye}/share/zsh/site-functions/_rye
+    if [ -f "$HOME/.rye/env" ]; then
+      source "$HOME/.rye/env"
+    fi
+  '';
 }
