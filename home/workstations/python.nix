f5ad23665db370627a6af25759048520cfbe697d
Daniel von Eßen <daniel@vonessen.eu>
2024-02-19 11:09:20 +0100

    feat: Added rye as the main python package manager.

--- home/workstations/python.nix
+++ home/workstations/python.nix
@@ -13,8 +13,22 @@ in {
     [
       pipenv
       pdm
+      rye
     ]
     ++ python311WithPackages;
+
+  programs.bash.initExtra = ''
+    if [ -f $HOME/.rye/env ]; then
+      source "$HOME/.rye/env"
+    fi
+    source ${pkgs.rye}/share/bash-completion/completions/rye.bash
+  '';
+  programs.zsh.initExtra = ''
+    if [ -f $HOME/.rye/env ]; then
+      source "$HOME/.rye/env"
+    fi
+    source ${pkgs.rye}/share/zsh/site-functions/_rye
+  '';
 
   programs.pyenv = {
     enable = true;
