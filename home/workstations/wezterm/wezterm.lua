ca9814a5ea0c127fa82dab0c628737dbdba238ba
Daniel von Eßen <daniel@vonessen.eu>
2024-02-25 10:17:23 +0100

    fix: Added mouse appearance bugfix from issue https://github.com/wez/wezterm/issues/1742

--- home/workstations/wezterm/wezterm.lua
+++ home/workstations/wezterm/wezterm.lua
@@ -18,9 +18,26 @@ function scheme_for_appearance(appearance)
   end
 end
 
+-- https://github.com/wez/wezterm/issues/1742#issuecomment-1075333507
+-- If coursor is not visible in gnome sessions on wayland
+local xcursor_size = nil
+local xcursor_theme = nil
 
--- For example, changing the color scheme:
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+if success then
+  xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+end
+
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+if success then
+  xcursor_size = tonumber(stdout)
+end
+-- END
+
+-- Return config object
 return {
+  xcursor_theme = xcursor_theme,
+  xcursor_size = xcursor_size,
   scrollback_lines = 50000,
   color_scheme = scheme_for_appearance(get_appearance()),
   font = wezterm.font('FiraCode Nerd Font', { weight = 450, italic = false })
